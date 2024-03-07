d3b5438f48fe1512c0ad838c43a8ecb5b6c8c43c
SebastianN3 <navassebas@gmail.com>
2024-03-07 15:11:10 -0500

    Added module header files

---
+++ trackpad/arduino/Display.h
@@ -0,0 +1,18 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "ArduinoGraphics.h"
+#include "Arduino_LED_Matrix.h"
+
+class Display {
+    private:
+        ArduinoLEDMatrix matrix;
+
+    public:
+        Display();
+        void printText(char text[]);
+        void printImage()
+
+}
+
+#endif
